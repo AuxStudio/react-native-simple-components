c5d212467555cef5e09417369876d043af258813
Shaun Saker <info@shaunsaker.com>
2017-10-22 10:39:00 +0200

    ButtonIcon component

---
+++ src/components/ButtonIcon.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
+
+import styleConstants from "../assets/styleConstants";
+
+import Touchable from "./Touchable";
+
+const styles = StyleSheet.create({
+    button: {
+        ...styleConstants.regularShadow,
+        backgroundColor: styleConstants.primary,
+        padding: 16,
+        borderRadius: 36,
+        borderWidth: 1,
+        borderColor: styleConstants.white,
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+    },
+    icon: {
+        fontSize: styleConstants.iconFont,
+        color: styleConstants.white,
+    },
+    disabled: {
+        opacity: 0.33,
+    },
+});
+
+export default (ButtonIcon = props => {
+    /*
+        PROPTYPES
+        
+        iconName: PropTypes.string,
+        handlePress: PropTypes.func.isRequired,
+        disabled: PropTypes.bool,
+        customIcon: PropTypes.node,
+        
+        backgroundColor: PropTypes.string,
+        iconColor: PropTypes.string,
+    */
+
+    const backgroundColorStyles = {
+        backgroundColor: props.backgroundColor
+            ? props.backgroundColor
+            : styleConstants.primary,
+    };
+
+    const iconColorStyles = {
+        color: props.iconColor ? props.iconColor : styleConstants.white,
+    };
+
+    const borderColorStyles = {
+        borderColor: props.iconColor ? props.iconColor : styleConstants.white,
+    };
+
+    const icon = props.customIcon ? (
+        props.customIcon
+    ) : (
+        <MaterialIcon
+            name={props.iconName}
+            style={[styles.icon, iconColorStyles]}
+        />
+    );
+
+    const button = props.disabled ? (
+        <View
+            style={[
+                styles.button,
+                backgroundColorStyles,
+                borderColorStyles,
+                styles.disabled,
+            ]}>
+            {icon}
+        </View>
+    ) : (
+        <Touchable
+            onPress={props.handlePress}
+            style={[styles.button, backgroundColorStyles, borderColorStyles]}>
+            {icon}
+        </Touchable>
+    );
+
+    return button;
+});
