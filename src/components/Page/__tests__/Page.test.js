5696655ab67190e5c796fb790776147d36a269a5
Shaun Saker <shaun@aux.co.za>
2018-06-09 12:20:05 +0200

    Added Page tests

---
+++ src/components/Page/__tests__/Page.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { View } from 'react-native';
+
+import Page from '../Page';
+
+it('renders a Page', () => {
+  expect(
+    renderer.create(
+      <Page verticalCenter horizontalCenter style={{ backgroundColor: 'red' }}>
+        <View />
+      </Page>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a Page with testing prop', () => {
+  expect(
+    renderer.create(
+      <Page testing style={{ backgroundColor: 'red' }}>
+        <View />
+      </Page>,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a Page with no props', () => {
+  expect(renderer.create(<Page />)).toMatchSnapshot();
+});
+/* eslint-enable */
