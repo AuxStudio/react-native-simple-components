daa776720e1bf1912ef2393f334e14ec2422f26e
Shaun Saker <shaun@aux.co.za>
2018-06-09 11:07:55 +0200

    Add test for ButtonIcon

---
+++ tests/ButtonIcon.spec.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+import ButtonIcon from '../src/components/ButtonIcon';
+
+it('renders a ButtonIcon', () => {
+  expect(
+    renderer.create(
+      <ButtonIcon
+        iconName="check"
+        handlePress={jest.fn()}
+        androidRipple
+        androidRippleColor="red"
+        androidRippleBorderless
+        showShadow
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a ButtonIcon with a customIcon', () => {
+  expect(
+    renderer.create(<ButtonIcon customIcon={<MaterialIcon name="check" />} />),
+  ).toMatchSnapshot();
+});
+
+it('renders a disabled ButtonIcon', () => {
+  expect(renderer.create(<ButtonIcon disabled />)).toMatchSnapshot();
+});
+
+/* eslint-enable */
