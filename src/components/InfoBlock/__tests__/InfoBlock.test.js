e3fab2322107af6cbf31105b218cee6db18405fc
Shaun Saker <shaun@aux.co.za>
2018-06-09 12:12:39 +0200

    Added InfoBlock tests

---
+++ src/components/InfoBlock/__tests__/InfoBlock.test.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import InfoBlock from '../InfoBlock';
+
+it('renders a InfoBlock', () => {
+  expect(
+    renderer.create(
+      <InfoBlock
+        title="Title"
+        description="Description"
+        titleTextStyle={{ color: 'red' }}
+        descriptionTextStyle={{ color: 'red' }}
+        style={{ backgroundColor: 'blue' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a InfoBlock with no props', () => {
+  expect(renderer.create(<InfoBlock />)).toMatchSnapshot();
+});
+/* eslint-enable */
