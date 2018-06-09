9cf9fb19aa71e6774327d86d9cfbd5645170eac2
Shaun Saker <shaun@aux.co.za>
2018-06-09 12:45:07 +0200

    Added StarRating tests

---
+++ src/components/StarRating/__tests__/StarRating.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import StarRating from '../StarRating';
+
+// TODO: it should return a value based on index on press
+it('renders a StarRating', () => {
+  expect(
+    renderer.create(
+      <StarRating
+        rating={3}
+        handlePress={jest.fn()}
+        androidRipple
+        androidRippleColor="red"
+        androidRippleBorderless
+        iconStyle={{ color: 'red' }}
+        style={{ backgroundColor: 'blue' }}
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a StarRating with no props', () => {
+  expect(renderer.create(<StarRating />)).toMatchSnapshot();
+});
+/* eslint-enable */
