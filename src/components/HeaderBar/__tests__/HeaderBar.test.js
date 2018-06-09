22ba3c52f4501479934c34faf079b97f693588c9
Shaun Saker <shaun@aux.co.za>
2018-06-09 11:46:17 +0200

    Add tests for HeaderBar

---
+++ src/components/HeaderBar/__tests__/HeaderBar.test.js
@@ -0,0 +1,53 @@
+/* eslint-disable import/first */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+
+import HeaderBar from '../HeaderBar';
+
+it('renders a HeaderBar', () => {
+  expect(
+    renderer.create(
+      <HeaderBar
+        showShadow
+        statusBarColor="red"
+        statusBarStyle="dark-content"
+        leftIconName="check"
+        handleLeftIconPress={jest.fn()}
+        text="Test"
+        rightIconName="close"
+        handleRightIconPress={jest.fn()}
+        androidRipple
+        androidRippleColor="red"
+        androidRippleBorderless
+      />,
+    ),
+  ).toMatchSnapshot();
+});
+
+it('renders a HeaderBar with a custom left component', () => {
+  expect(
+    renderer.create(<HeaderBar leftComponent={<MaterialIcon name="check" />} />),
+  ).toMatchSnapshot();
+});
+it('renders a HeaderBar with a custom text component', () => {
+  expect(
+    renderer.create(<HeaderBar textComponent={<MaterialIcon name="check" />} />),
+  ).toMatchSnapshot();
+});
+it('renders a HeaderBar with a custom right component', () => {
+  expect(
+    renderer.create(<HeaderBar rightComponent={<MaterialIcon name="check" />} />),
+  ).toMatchSnapshot();
+});
+it('renders a HeaderBar with a text left', () => {
+  expect(renderer.create(<HeaderBar text="Test" textLeft />)).toMatchSnapshot();
+});
+it('renders a HeaderBar with a text left', () => {
+  expect(renderer.create(<HeaderBar text="Test" textRight />)).toMatchSnapshot();
+});
+
+it('renders a HeaderBar with no props', () => {
+  expect(renderer.create(<HeaderBar />)).toMatchSnapshot();
+});
+/* eslint-enable */
