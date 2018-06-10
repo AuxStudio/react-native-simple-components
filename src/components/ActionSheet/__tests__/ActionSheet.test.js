33bab1fb366c1e5778becb577275a66fc54fa591
Shaun Saker <shaun@aux.co.za>
2018-06-10 09:17:20 +0200

    Update ActionSheet test

--- src/components/ActionSheet/__tests__/ActionSheet.test.js
+++ src/components/ActionSheet/__tests__/ActionSheet.test.js
@@ -20,14 +20,26 @@ it('renders a ActionSheet', () => {
     />,
   );
   expect(component).toMatchSnapshot();
-
-  const instance = component.getInstance();
-  instance.handleSelect({ iconName: 'check', text: 'Test 1' });
-
-  expect(instance.state.shouldAnimateOut).toBe(true);
 });
 
 it('renders a ActionSheet with no props', () => {
   expect(renderer.create(<ActionSheet />)).toMatchSnapshot();
+});
+
+it('renders a ActionSheet and selects a menu item', () => {
+  const component = renderer.create(
+    <ActionSheet
+      options={[{ iconName: 'check', text: 'Test 1' }, { iconName: 'close', text: 'Test 2' }]}
+      handlePress={jest.fn()}
+      rowHeight={100}
+      textStyle={{ color: 'red' }}
+      iconStyle={{ color: 'red' }}
+      style={{ backgroundColor: 'blue' }}
+    />,
+  );
+  const instance = component.getInstance();
+
+  instance.handleSelect({ iconName: 'check', text: 'Test 1' });
+  expect(instance.state.shouldAnimateOut).toBe(true);
 });
 /* eslint-enable */
