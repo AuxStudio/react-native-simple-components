bbbeb5477db902c05077af0c8738492db6232b7b
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:47:48 +0200

    Update ActionSheet tests

--- src/components/ActionSheet/__tests__/ActionSheet.test.js
+++ src/components/ActionSheet/__tests__/ActionSheet.test.js
@@ -7,11 +7,13 @@ jest.mock('react-native-simple-animators', () => ({
 
 import ActionSheet from '../'; // eslint-disable-line
 
+const OPTIONS = [{ iconName: 'check', text: 'Test 1' }, { iconName: 'close', text: 'Test 2' }];
+
 describe('ActionSheet', () => {
   it('renders with all props', () => {
     const component = renderer.create(
       <ActionSheet
-        options={[{ iconName: 'check', text: 'Test 1' }, { iconName: 'close', text: 'Test 2' }]}
+        options={OPTIONS}
         handlePress={jest.fn()}
         rowHeight={100}
         textStyle={{ color: 'red' }}
@@ -26,20 +28,27 @@ describe('ActionSheet', () => {
     expect(renderer.create(<ActionSheet />)).toMatchSnapshot();
   });
 
-  // it('renders and selects a menu item', () => {
-  //   const component = renderer.create(
-  //     <ActionSheet
-  //       options={[{ iconName: 'check', text: 'Test 1' }, { iconName: 'close', text: 'Test 2' }]}
-  //       handlePress={jest.fn()}
-  //       rowHeight={100}
-  //       textStyle={{ color: 'red' }}
-  //       iconStyle={{ color: 'red' }}
-  //       style={{ backgroundColor: 'blue' }}
-  //     />,
-  //   );
-  //   const instance = component.getInstance();
+  it('renders and selects a menu item', (done) => {
+    const handlePress = jest.fn();
+    const component = renderer.create(
+      <ActionSheet
+        options={OPTIONS}
+        handlePress={handlePress}
+        rowHeight={100}
+        textStyle={{ color: 'red' }}
+        iconStyle={{ color: 'red' }}
+        style={{ backgroundColor: 'blue' }}
+      />,
+    );
+    const instance = component.getInstance();
 
-  //   instance.handleSelect({ iconName: 'check', text: 'Test 1' });
-  //   expect(instance.state.shouldAnimateOut).toBe(true);
-  // });
+    instance.handleSelect(OPTIONS[0]);
+    expect(instance.state.shouldAnimateOut).toBe(true);
+
+    setTimeout(() => {
+      expect(handlePress).toMatchSnapshot();
+
+      done();
+    }, 500);
+  });
 });
