eb230c09a1a0675a66bd7201e93165f32e521660
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:33:08 +0200

    Update NotificationIcon tests

--- src/components/NotificationIcon/__tests__/NotificationIcon.test.js
+++ src/components/NotificationIcon/__tests__/NotificationIcon.test.js
@@ -3,19 +3,21 @@ import renderer from 'react-test-renderer';
 
 import NotificationIcon from '../';
 
-it('renders a NotificationIcon', () => {
-  expect(
-    renderer.create(
-      <NotificationIcon
-        count={5}
-        showShadow
-        textStyle={{ color: 'red' }}
-        style={{ backgroundColor: 'blue' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('NotificationIcon', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <NotificationIcon
+          count={5}
+          showShadow
+          textStyle={{ color: 'red' }}
+          style={{ backgroundColor: 'blue' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a NotificationIcon with no props', () => {
-  expect(renderer.create(<NotificationIcon />)).toMatchSnapshot();
+  it('renders with minimum required props', () => {
+    expect(renderer.create(<NotificationIcon />)).toMatchSnapshot();
+  });
 });
