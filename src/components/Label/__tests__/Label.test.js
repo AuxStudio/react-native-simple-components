66ba864121dd4f5b18932c8820e075e256e9d6c9
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:30:37 +0200

    Update Label tests

--- src/components/Label/__tests__/Label.test.js
+++ src/components/Label/__tests__/Label.test.js
@@ -1,31 +1,31 @@
-/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import Label from '../';
 
-it('renders a Label', () => {
-  expect(
-    renderer.create(
-      <Label
-        handlePress={jest.fn()}
-        iconName="check"
-        text="Test"
-        showShadow
-        iconStyle={{ color: 'red' }}
-        textStyle={{ color: 'red' }}
-        style={{ backgroundColor: 'blue' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('Label', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <Label
+          handlePress={jest.fn()}
+          iconName="check"
+          text="Test"
+          showShadow
+          iconStyle={{ color: 'red' }}
+          textStyle={{ color: 'red' }}
+          style={{ backgroundColor: 'blue' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a Label with custom icon', () => {
-  expect(renderer.create(<Label customIcon={<MaterialIcon name="check" />} />)).toMatchSnapshot();
-});
+  it('renders with minimum required props', () => {
+    expect(renderer.create(<Label />)).toMatchSnapshot();
+  });
 
-it('renders a Label with no props', () => {
-  expect(renderer.create(<Label />)).toMatchSnapshot();
+  it('renders with custom icon', () => {
+    expect(renderer.create(<Label customIcon={<MaterialIcon name="check" />} />)).toMatchSnapshot();
+  });
 });
-/* eslint-enable */
