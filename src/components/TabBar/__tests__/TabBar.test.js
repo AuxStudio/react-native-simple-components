76e687c594ec8bbf4414b44da2bd04337924b8e5
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:40:45 +0200

    Update TabBar tests

--- src/components/TabBar/__tests__/TabBar.test.js
+++ src/components/TabBar/__tests__/TabBar.test.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
@@ -18,52 +17,57 @@ const TABS = [
 ];
 const ACTIVE_TAB = 'Home';
 
-it('renders a TabBar', () => {
-  expect(
-    renderer.create(
-      <TabBar
-        tabs={TABS}
-        activeTab={ACTIVE_TAB}
-        handlePress={jest.fn()}
-        androidRipple
-        androidRippleColor="red"
-        androidRippleBorderless
-        showShadow
-        textColor="red"
-        activeTextColor="blue"
-        iconStyle={{ color: 'red' }}
-        textStyle={{ color: 'red' }}
-        tabStyle={{ backgroundColor: 'blue' }}
-        activeTabStyle={{ backgroundColor: 'green' }}
-        disabledTabStyle={{ backgroundColor: 'purple' }}
-        style={{ backgroundColor: 'orange' }}
-        wrapperStyle={{ backgroundColor: 'pink' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('TabBar', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <TabBar
+          tabs={TABS}
+          activeTab={ACTIVE_TAB}
+          handlePress={jest.fn()}
+          androidRipple
+          androidRippleColor="red"
+          androidRippleBorderless
+          showShadow
+          textColor="red"
+          activeTextColor="blue"
+          iconStyle={{ color: 'red' }}
+          textStyle={{ color: 'red' }}
+          tabStyle={{ backgroundColor: 'blue' }}
+          activeTabStyle={{ backgroundColor: 'green' }}
+          disabledTabStyle={{ backgroundColor: 'purple' }}
+          style={{ backgroundColor: 'orange' }}
+          wrapperStyle={{ backgroundColor: 'pink' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a TabBar with customIcon', () => {
-  expect(
-    renderer.create(
-      <TabBar
-        tabs={[
-          {
-            title: 'Home',
-            customIcon: <MaterialIcon name="check" />,
-          },
-        ]}
-        activeTab={ACTIVE_TAB}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with minimum required props', () => {
+    expect(renderer.create(<TabBar tabs={TABS} activeTab={ACTIVE_TAB} />)).toMatchSnapshot();
+  });
 
-it('renders a TabBar', () => {
-  expect(
-    renderer.create(
-      <TabBar tabs={TABS} activeTab={ACTIVE_TAB} shouldScrollHorizontally tabWidth={200} />,
-    ),
-  ).toMatchSnapshot();
+  it('renders with customIcon', () => {
+    expect(
+      renderer.create(
+        <TabBar
+          tabs={[
+            {
+              title: 'Home',
+              customIcon: <MaterialIcon name="check" />,
+            },
+          ]}
+          activeTab={ACTIVE_TAB}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('renders horizontally', () => {
+    expect(
+      renderer.create(
+        <TabBar tabs={TABS} activeTab={ACTIVE_TAB} shouldScrollHorizontally tabWidth={200} />,
+      ),
+    ).toMatchSnapshot();
+  });
 });
-/* eslint-enable */
