9af45614839ac611bbc7c74d4279f912cc6d79fe
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:41:47 +0200

    Update Touchable tests

--- src/components/Touchable/__tests__/Touchable.test.js
+++ src/components/Touchable/__tests__/Touchable.test.js
@@ -1,48 +1,39 @@
-/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { View } from 'react-native';
 
 import Touchable from '../';
 
-it('renders a Touchable', () => {
-  expect(
-    renderer.create(
-      <Touchable
-        onPress={jest.fn()}
-        onLongPress={jest.fn()}
-        androidRipple
-        androidRippleColor="red"
-        androidRippleBorderless
-        style={{ backgroundColor: 'red' }}
-      >
-        <View />
-      </Touchable>,
-    ),
-  ).toMatchSnapshot();
-});
+describe('Touchable', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <Touchable
+          onPress={jest.fn()}
+          onLongPress={jest.fn()}
+          androidRipple
+          androidRippleColor="red"
+          androidRippleBorderless
+          disabled
+          style={{ backgroundColor: 'red' }}
+        >
+          <View />
+        </Touchable>,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a Touchable with no feedback', () => {
-  expect(
-    renderer.create(
-      <Touchable disabledFeedback>
-        <View />
-      </Touchable>,
-    ),
-  ).toMatchSnapshot();
-});
+  it('renders with minimum required props', () => {
+    expect(renderer.create(<Touchable />)).toMatchSnapshot();
+  });
 
-it('renders a disabled Touchable', () => {
-  expect(
-    renderer.create(
-      <Touchable disabled>
-        <View />
-      </Touchable>,
-    ),
-  ).toMatchSnapshot();
+  it('renders with no feedback', () => {
+    expect(
+      renderer.create(
+        <Touchable disabledFeedback>
+          <View />
+        </Touchable>,
+      ),
+    ).toMatchSnapshot();
+  });
 });
-
-it('renders a Touchable with no props', () => {
-  expect(renderer.create(<Touchable />)).toMatchSnapshot();
-});
-/* eslint-enable */
