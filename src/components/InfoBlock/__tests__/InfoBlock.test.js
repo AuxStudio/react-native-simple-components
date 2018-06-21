b8fa4a058025ab2efe42487bba14cef3ff3c5a14
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:27:38 +0200

    Update InfoBlock tests

--- src/components/InfoBlock/__tests__/InfoBlock.test.js
+++ src/components/InfoBlock/__tests__/InfoBlock.test.js
@@ -1,24 +1,24 @@
-/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import InfoBlock from '../';
 
-it('renders a InfoBlock', () => {
-  expect(
-    renderer.create(
-      <InfoBlock
-        title="Title"
-        description="Description"
-        titleTextStyle={{ color: 'red' }}
-        descriptionTextStyle={{ color: 'red' }}
-        style={{ backgroundColor: 'blue' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('InfoBlock', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <InfoBlock
+          title="Title"
+          description="Description"
+          titleTextStyle={{ color: 'red' }}
+          descriptionTextStyle={{ color: 'red' }}
+          style={{ backgroundColor: 'blue' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a InfoBlock with no props', () => {
-  expect(renderer.create(<InfoBlock />)).toMatchSnapshot();
+  it('renders with maximum required props', () => {
+    expect(renderer.create(<InfoBlock />)).toMatchSnapshot();
+  });
 });
-/* eslint-enable */
