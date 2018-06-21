80ec1063184202e701dd3edc74c4d0d1316cb321
Shaun Saker <shaun@aux.co.za>
2018-06-21 08:38:00 +0200

    Update StarRating tests

--- src/components/StarRating/__tests__/StarRating.test.js
+++ src/components/StarRating/__tests__/StarRating.test.js
@@ -1,27 +1,27 @@
-/* eslint-disable import/first */
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import StarRating from '../';
 
 // TODO: it should return a value based on index on press
-it('renders a StarRating', () => {
-  expect(
-    renderer.create(
-      <StarRating
-        rating={3}
-        handlePress={jest.fn()}
-        androidRipple
-        androidRippleColor="red"
-        androidRippleBorderless
-        iconStyle={{ color: 'red' }}
-        style={{ backgroundColor: 'blue' }}
-      />,
-    ),
-  ).toMatchSnapshot();
-});
+describe('StarRating', () => {
+  it('renders with all props', () => {
+    expect(
+      renderer.create(
+        <StarRating
+          rating={3}
+          handlePress={jest.fn()}
+          androidRipple
+          androidRippleColor="red"
+          androidRippleBorderless
+          iconStyle={{ color: 'red' }}
+          style={{ backgroundColor: 'blue' }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
 
-it('renders a StarRating with no props', () => {
-  expect(renderer.create(<StarRating />)).toMatchSnapshot();
+  it('renders with minimum required props', () => {
+    expect(renderer.create(<StarRating />)).toMatchSnapshot();
+  });
 });
-/* eslint-enable */
