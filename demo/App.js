151c4f0f5f753192a9c03cd75be7d337650cae8f
shaunsaker <shaun@aux.co.za>
2018-06-30 13:19:53 +0200

    Update App to render components

--- demo/App.js
+++ demo/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Button } from 'react-native-simple-components';
+import { View, ScrollView, Text } from 'react-native';
+import {
+  Button,
+  ButtonIcon,
+  CheckBox,
+  DeleteButton,
+  HeaderBar,
+  IconTextRow,
+  InfoBlock,
+  Label,
+  Loader,
+  LoadingText,
+  NotificationIcon,
+  RotatingChevron,
+  SmartImage,
+  StarRating,
+  TabBar,
+} from 'react-native-simple-components';
 
 import styles from './styles';
 
@@ -8,13 +24,113 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.setSelected = this.setSelected.bind(this);
+
+    this.components = [
+      {
+        name: 'Button',
+        component: Button,
+      },
+      {
+        name: 'ButtonIcon',
+        component: ButtonIcon,
+      },
+      {
+        name: 'CheckBox',
+        component: CheckBox,
+      },
+      {
+        name: 'DeleteButton',
+        component: DeleteButton,
+      },
+      {
+        name: 'HeaderBar',
+        component: HeaderBar,
+      },
+      {
+        name: 'IconTextRow',
+        component: IconTextRow,
+      },
+      {
+        name: 'InfoBlock',
+        component: InfoBlock,
+      },
+      {
+        name: 'Label',
+        component: Label,
+      },
+      {
+        name: 'Loader',
+        component: Loader,
+      },
+      {
+        name: 'LoadingText',
+        component: LoadingText,
+      },
+      {
+        name: 'NotificationIcon',
+        component: NotificationIcon,
+      },
+      {
+        name: 'RotatingChevron',
+        component: RotatingChevron,
+      },
+      {
+        name: 'SmartImage',
+        component: SmartImage,
+      },
+      {
+        name: 'StarRating',
+        component: StarRating,
+      },
+      {
+        name: 'TabBar',
+        component: TabBar,
+      },
+    ];
+
+    this.state = {
+      selected: this.components[0].name,
+    };
+  }
+
+  setSelected(selected) {
+    this.setState({
+      selected,
+    });
   }
 
   render() {
+    const { selected } = this.state;
+    let SelectedComponent;
+
+    this.components.forEach((component) => {
+      if (component.name === selected) {
+        SelectedComponent = component.component;
+      }
+    });
+
     return (
       <View style={styles.container}>
-        <View />
+        <View style={styles.demoContainer}>
+          <SelectedComponent />
+        </View>
+        <Text style={styles.titleText}>Select a component</Text>
+        <ScrollView
+          style={styles.scrollContainer}
+          contentContainerStyle={styles.scrollContentContainer}
+        >
+          {this.components.map((component) => {
+            return (
+              <View key={component.name} style={styles.buttonContainer}>
+                <Button
+                  text={component.name}
+                  handlePress={() => this.setSelected(component.name)}
+                />
+              </View>
+            );
+          })}
+        </ScrollView>
       </View>
     );
   }
--- demo/styles.js
+++ demo/styles.js
@@ -1,7 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
+
+const PADDING = 20;
+const BORDER_RADIUS = 5;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 24 : 0, // clear the status bar
+    padding: PADDING,
+    paddingBottom: 0, // scroll container
+  },
+  demoContainer: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: BORDER_RADIUS,
+    marginBottom: PADDING,
+  },
+
+  scrollContainer: {
+    flex: 1, // fill the remaining space
+  },
+  scrollContentContainer: {
+    paddingBottom: PADDING,
+  },
+
+  titleText: {
+    fontSize: 18,
+    fontWeight: '700',
+    marginBottom: PADDING,
+  },
+
+  buttonContainer: {
+    marginBottom: PADDING / 2,
+  },
 });
 
 export default styles;
